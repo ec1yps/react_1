77fc78a24194728cd3035e5b5b9f70f9410975b9
ec1yps_ <ec1yps@yandex.ru>
2025-08-27 04:41:39 +1000

    DB json

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import Header from './header/Header.js';
 import Footer from './footer/Footer.js';
 import Article from './article/Article.js';
 import Nav from './nav/Nav.js';
+import data from './db.json';
 
 function App(props) {
+	let db = data.people;
 	return (
 		<div className="App">
 			<Header />
 			<Nav navigation={props.navigation} />
-			<Article title="Современные десктопные процессоры архитектуры x86 общие принципы работы (x86 CPU digest 2.0)"
-				content="Здесь будет содержимое статьи" />
+			<Article db={db} />
+			{/* <Article title="Современные десктопные процессоры архитектуры x86 общие принципы работы (x86 CPU digest 2.0)"
+				content="Здесь будет содержимое статьи" /> */}
 			<Footer />
 		</div>
 	);
